344d6ee202c04b9a1c579c3838504f859422ac71
Max Gordon <max@gforge.se>
2014-12-22 21:05:02 +0100

    Moved functions from Gmisc

---
+++ R/forestplot-package.R
@@ -0,0 +1,8 @@
+#' The forest plot function, \code{\link{forestplot2}}, is a more general
+#' version of the original \pkg{rmeta}-packages \code{\link[rmeta]{forestplot}}
+#' implementation. The aim is at using forest plots for more than
+#' just meta-analyses.
+#'
+#' @name forestplot-package
+#' @docType package
+NULL
