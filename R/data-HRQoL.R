ae2617d45ceaf844f68137c9ea4bc857e79dd608
Max Gordon <max@gforge.se>
2014-12-23 16:14:01 +0100

    Added data

---
+++ R/data-HRQoL.R
@@ -0,0 +1,11 @@
+#' Coefficients and confidence interval for HRQoL in Sweden
+#'
+#' The data is a list containing the Swedish and the Danish coefficients
+#' for health related quality of life 1 year after surgery. The age is modelled
+#' as a spline and is therefore presented as a contrast.
+#'
+#' @name HRQoL
+#' @docType data
+#' @author Max Gordon \email{max@@gforge.se}
+#' @keywords data
+NULL
